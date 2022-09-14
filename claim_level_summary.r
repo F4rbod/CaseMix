0703dbfd5af78bf7695953679054e1b2d1c04843
F4rbod <farbodaline@gmail.com>
2022-09-14 11:53:59 -0400

    redid the analyses using the claim level data

--- claim_level_summary.r
+++ claim_level_summary.r
@@ -2,7 +2,7 @@ options(repr.matrix.max.rows = 100, repr.matrix.max.cols = 300)
 options(repr.plot.width = 20, repr.plot.height = 15)
 options(width = 300)
 
-numcores <- 64
+numcores <- 10
 
 library(tidyverse)
 library(data.table)
